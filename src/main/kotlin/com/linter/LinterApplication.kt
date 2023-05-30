de7e85d227baf6565663e278f2c7919ee970e681
matiaslcoulougian <matias.coulougian@ing.austral.edu.ar>
2023-05-30 07:58:56 -0300

    Configure linter app

---
+++ src/main/kotlin/com/linter/LinterApplication.kt
@@ -0,0 +1,33 @@
+package com.linter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Main entry point for the application.
+ * @property args The command line arguments.
+ */
+@SpringBootApplication
+class LinterApplication
+
+/**
+ * Main entry point for the application.
+ */
+fun main(args: Array<String>) {
+    runApplication<LinterApplication>(*args)
+}
+/**
+ * Controller for the application.
+ * @property name The name of the person to greet.
+ */
+@RestController
+class MessageController {
+    /**
+     * Returns a greeting for the given name.
+     */
+    @GetMapping("/")
+    fun index(@RequestParam("name") name: String): String = "Hello, $name!"
+}
